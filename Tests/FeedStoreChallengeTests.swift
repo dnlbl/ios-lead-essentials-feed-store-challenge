ab7f16c3e768e8244a58011c5feb823623dfb173
Васякин Данил Евгеньевич <Danil.Vassyakin@kaspi.kz>
2020-09-23 18:42:15 +0600

    passing assertThatDeleteDeliversNoErrorOnEmptyCache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -72,9 +72,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	}
 
 	func test_delete_deliversNoErrorOnEmptyCache() {
-//		let sut = makeSUT()
-//
-//		assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
 	}
 
 	func test_delete_hasNoSideEffectsOnEmptyCache() {
